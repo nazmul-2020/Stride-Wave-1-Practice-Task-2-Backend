ab01d4d29ba00577e98e39e25200f3a80517b027
nazmul-2020 <nazmulcpi2012@gmail.com>
2024-06-02 13:31:32 +0600

    added all data get ,post $sing get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 5000;
 
 app.use(cors());
@@ -31,10 +31,32 @@ async function run() {
     app.post("/shoes", async (req, res) => {
       const shoesData = req.body;
       const result = await shoesCollection.insertOne(shoesData);
+      res.send(result);
+    });
+
+    app.get("/shoes", async (req, res) => {
+      const shoesData = shoesCollection.find();
+      const result = await shoesData.toArray();
+      res.send(result);
+    });
+
+    app.get("/shoes/:id", async (req, res) => {
+      const id = req.params.id;
+      const shoesData = await shoesCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(shoesData);
+    });
+
+    app.patch("/shoes/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const result = await shoesCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      );
       res.send(result)
     });
-    
-
 
     console.log("DataBase is Connect");
   } finally {
